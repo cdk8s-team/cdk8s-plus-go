283f70809e6a093301da6595ac9f10dc561aed1c
cdk8s-automation <cdk8s-team@amazon.com>
2021-11-30 06:35:41 +0000

    chore(release): v1.0.0-beta.54

--- cdk8splus22/cdk8splus22.go
+++ cdk8splus22/cdk8splus22.go
@@ -1,4 +1,4 @@
-// High level abstractions on top of cdk8s
+// cdk8s+ is a software development framework that provides high level abstractions for authoring Kubernetes applications. cdk8s-plus-22 synthesizes Kubernetes manifests for Kubernetes 1.22.0
 package cdk8splus22
 
 import (
