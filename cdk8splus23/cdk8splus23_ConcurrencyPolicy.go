73fe68d3a6766d9954521ada0f89e339f40b80d4
cdk8s-automation <cdk8s-team@amazon.com>
2022-09-13 00:14:11 +0000

    chore(release): v2.0.0-rc.110

---
+++ cdk8splus23/cdk8splus23_ConcurrencyPolicy.go
@@ -0,0 +1,18 @@
+// cdk8s+ is a software development framework that provides high level abstractions for authoring Kubernetes applications. cdk8s-plus-23 synthesizes Kubernetes manifests for Kubernetes 1.23.0
+package cdk8splus23
+
+
+// Concurrency policy for CronJobs.
+type ConcurrencyPolicy string
+
+const (
+	// This policy allows to run job concurrently.
+	ConcurrencyPolicy_ALLOW ConcurrencyPolicy = "ALLOW"
+	// This policy does not allow to run job concurrently.
+	//
+	// It does not let a new job to be scheduled if the previous one is not finished yet.
+	ConcurrencyPolicy_FORBID ConcurrencyPolicy = "FORBID"
+	// This policy replaces the currently running job if a new job is being scheduled.
+	ConcurrencyPolicy_REPLACE ConcurrencyPolicy = "REPLACE"
+)
+
---
+++ cdk8splus23/cdk8splus23_CronJob__no_runtime_type_checking.go
@@ -0,0 +1,32 @@
+//go:build no_runtime_type_checking
+// +build no_runtime_type_checking
+
+// cdk8s+ is a software development framework that provides high level abstractions for authoring Kubernetes applications. cdk8s-plus-23 synthesizes Kubernetes manifests for Kubernetes 1.23.0
+package cdk8splus23
+
+// Building without runtime type checking enabled, so all the below just return nil
+
+func (c *jsiiProxy_CronJob) validateAddContainerParameters(cont *ContainerProps) error {
+	return nil
+}
+
+func (c *jsiiProxy_CronJob) validateAddHostAliasParameters(hostAlias *HostAlias) error {
+	return nil
+}
+
+func (c *jsiiProxy_CronJob) validateAddInitContainerParameters(cont *ContainerProps) error {
+	return nil
+}
+
+func (c *jsiiProxy_CronJob) validateAddVolumeParameters(vol Volume) error {
+	return nil
+}
+
+func validateCronJob_IsConstructParameters(x interface{}) error {
+	return nil
+}
+
+func validateNewCronJobParameters(scope constructs.Construct, id *string, props *CronJobProps) error {
+	return nil
+}
+
