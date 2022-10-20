5353be8f97e9096c31ad710b6afcc20f7451d56f
cdk8s-automation <cdk8s-team@amazon.com>
2022-10-20 03:06:08 +0000

    chore(release): v2.0.0-rc.159

--- cdk8splus24/cdk8splus24_Pod__no_runtime_type_checking.go
+++ cdk8splus24/cdk8splus24_Pod__no_runtime_type_checking.go
@@ -1,5 +1,4 @@
 //go:build no_runtime_type_checking
-// +build no_runtime_type_checking
 
 // cdk8s+ is a software development framework that provides high level abstractions for authoring Kubernetes applications. cdk8s-plus-24 synthesizes Kubernetes manifests for Kubernetes 1.24.0
 package cdk8splus24
