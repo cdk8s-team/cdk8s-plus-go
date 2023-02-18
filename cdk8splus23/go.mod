246fc63eb2d806d37649e92a8bbc06165ed8a2ab
cdk8s-automation <cdk8s-team@amazon.com>
2023-02-18 02:36:22 +0000

    chore(release): v2.4.25

--- cdk8splus23/go.mod
+++ cdk8splus23/go.mod
@@ -4,6 +4,6 @@ go 1.18
 
 require (
 	github.com/aws/jsii-runtime-go v1.75.0
-	github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2 v2.7.3
-	github.com/aws/constructs-go/constructs/v10 v10.1.253
+	github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2 v2.7.4
+	github.com/aws/constructs-go/constructs/v10 v10.1.254
 )
--- cdk8splus23/jsii/jsii.go
+++ cdk8splus23/jsii/jsii.go
@@ -13,7 +13,7 @@ import (
 	cdk8s      "github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2/jsii"
 )
 
-//go:embed cdk8s-plus-23-2.4.24.tgz
+//go:embed cdk8s-plus-23-2.4.25.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -24,5 +24,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("cdk8s-plus-23", "2.4.24", tarball)
+	_jsii_.Load("cdk8s-plus-23", "2.4.25", tarball)
 }
