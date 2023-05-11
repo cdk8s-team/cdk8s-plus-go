882a402b80764be89cf2049360186406dc57a2e1
cdk8s-automation <cdk8s-team@amazon.com>
2023-05-11 02:25:16 +0000

    chore(release): v2.7.25

--- cdk8splus24/IScalable__checks.go
+++ cdk8splus24/IScalable__checks.go
@@ -1,6 +1,5 @@
 //go:build !no_runtime_type_checking
 
-// cdk8s+ is a software development framework that provides high level abstractions for authoring Kubernetes applications. cdk8s-plus-24 synthesizes Kubernetes manifests for Kubernetes 1.24.0
 package cdk8splus24
 
 import (
--- cdk8splus24/PersistentVolumeReclaimPolicy.go
+++ cdk8splus24/PersistentVolumeReclaimPolicy.go
@@ -1,4 +1,3 @@
-// cdk8s+ is a software development framework that provides high level abstractions for authoring Kubernetes applications. cdk8s-plus-24 synthesizes Kubernetes manifests for Kubernetes 1.24.0
 package cdk8splus24
 
 
