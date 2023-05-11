be3df143565302569a9c359efb1985a43002803c
cdk8s-automation <cdk8s-team@amazon.com>
2023-05-11 02:28:33 +0000

    chore(release): v2.3.25

--- cdk8splus26/main.go
+++ cdk8splus26/main.go
@@ -1,3 +1,4 @@
+// cdk8s+ is a software development framework that provides high level abstractions for authoring Kubernetes applications. cdk8s-plus-26 synthesizes Kubernetes manifests for Kubernetes 1.26.0
 package cdk8splus26
 
 import (
